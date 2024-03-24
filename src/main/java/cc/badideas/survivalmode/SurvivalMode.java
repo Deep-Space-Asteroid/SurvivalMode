badb9bc950600c3a24361f634396e0a372f030e2
Micah <micah.apex@gmail.com>
2024-03-23 20:06:22 -0400

    Remove dependency on Lunar I'm not going to spend any more time trying to wrangle an undocumented library, we're doing this raw

--- src/main/java/cc/badideas/survivalmode/SurvivalMode.java
+++ src/main/java/cc/badideas/survivalmode/SurvivalMode.java
@@ -1,10 +1,5 @@
 package cc.badideas.survivalmode;
 
-import com.github.repletsin5.lunar.api.blocks.BlockEvents;
-import finalforeach.cosmicreach.gamestates.InGame;
-import finalforeach.cosmicreach.world.BlockPosition;
-import finalforeach.cosmicreach.world.World;
-import finalforeach.cosmicreach.world.blocks.BlockState;
 import net.fabricmc.api.ModInitializer;
 
 import java.util.logging.Logger;
@@ -15,9 +10,6 @@ public class SurvivalMode implements ModInitializer {
 
     @Override
     public void onInitialize() {
-//        BlockEvents.BEFORE_BLOCK_BREAK.register((world, blockPosition, v) -> {
-//            BlockState blockBroken = InGame.world.getBlockState(blockPosition.getGlobalX(), blockPosition.getGlobalY(), blockPosition.getGlobalZ());
-//            LOGGER.info(blockBroken.getBlockId());
-//        });
+        LOGGER.info("Survival Mode is loaded.");
     }
 }
